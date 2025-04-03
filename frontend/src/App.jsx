4483302bbfe94ed2170721a2f4cf93959527f0fc
MuhammadSadiqHanif <sadiq_hanif@gmail.com>
2025-04-03 18:10:56 +0500

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import { useState } from "react";
